e05373169eb6e46ed0818da1dbed7f8f978189b9
Mohammad Nabizade <mohamad.nabi1377@gmail.com>
2024-08-16 20:51:41 +0330

    Update group_norm.py
    the keras.engine module has been restructured in recent versions of TensorFlow/Keras. The Layer and InputSpec classes have been moved to different locations in the newer versions.

--- group_norm.py
+++ group_norm.py
@@ -1,4 +1,5 @@
-from keras.engine import Layer, InputSpec
+from keras.layers import Layer
+from keras.layers import InputSpec
 from keras import initializers
 from keras import regularizers
 from keras import constraints
